a2aeda9432b3e8b3cc0a4a4f6b01f2adbc9d0f50
Tania <tanja.zirael@gmail.com>
2022-06-20 22:02:11 +0300

    homework1 task1

--- academy/src/by/academy/homework1/Task1.java
+++ academy/src/by/academy/homework1/Task1.java
@@ -1,5 +1,68 @@
 package by.academy.homework1;
 
-public class Task1 {
+import java.util.Scanner;
 
+public class Task1 {
+	public static void main (String[] args) {
+		int discountpercent, totalsum;
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Введите сумму покупки: ");
+		if (sc.hasNextInt()) {
+			int sum=sc.nextInt();
+			if (sum<0) {
+				System.out.println("Сумма может быть только положительным числом");			
+			}
+			else {
+				System.out.println("Введите возвраст: ");
+				if (sc.hasNextInt()) {
+					int age=sc.nextInt();
+					if (age<=0) {
+						System.out.println("Возраст может быть только положительным числом");
+					}
+					else {
+						if (sum<100) {
+							discountpercent=5;
+							totalsum=sum*(100-discountpercent);
+							System.out.println("Финальная цена: "+totalsum);
+						}	
+						else if (sum>=100 && sum<200) {
+							discountpercent=7;
+							totalsum=sum*(100-discountpercent);
+							System.out.println("Финальная цена: "+totalsum);
+						}	
+						else if (sum>=200 && sum<300) {
+							if (age>18){
+								discountpercent=16;
+								totalsum=sum*(100-discountpercent);
+								System.out.println("Финальная цена: "+totalsum);
+							}
+							else {
+								discountpercent=9;
+								totalsum=sum*(100-discountpercent);
+								System.out.println("Финальная цена: "+totalsum);
+							}	
+						}	
+						else if (sum>=300 && sum<400) {
+							discountpercent=15;
+							totalsum=sum*(100-discountpercent);
+							System.out.println("Финальная цена: "+totalsum);
+						}
+						else if (sum>=400) {
+							discountpercent=20;
+							totalsum=sum*(100-discountpercent);
+							System.out.println("Финальная цена: "+totalsum);
+						}	
+					}
+				}	
+				else {
+					System.out.println("Необходимо вводить только цифры");
+				}	
+			}	
+		}			
+		else {
+			System.out.println("Необходимо вводить только цифры");	
+		}	
+		sc.close();		
+	}
 }
