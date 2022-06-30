60924bada7c548d0fd5373892678d92383f24d87
Tania <tanja.zirael@gmail.com>
2022-06-30 14:25:53 +0300

    homework2

--- academy/src/main/java/by/academy/homework2/Task2.java
+++ academy/src/main/java/by/academy/homework2/Task2.java
@@ -1,16 +1,31 @@
 package by.academy.homework2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task2 {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		int n=0;
-		String[] str = new String[n];
-		for (int i=0;i<=n; i++ ) {
-		System.out.println("Введите слово: ");
-		str[i]=sc.next();}
-
+		System.out.println("Введите число n (количество слов): ");
+		if (sc.hasNextInt()) {
+			int n=sc.nextInt();
+			String [] array=new String [n];
+			System.out.println("Введите cлова: ");
+//			
+		for (int i=0;i<=n-1; i++ ) {		
+			String str=sc.next();
+			array[i]=str;			
+		}
+//		for (int i=0;i<=n-1; i++ ) {
+			System.out.println(Arrays.toString(array));
+//		}
+		for (int i=0;i<=n-1; i++ ) {
+			System.out.println(array[i]);
+		}			
+		}
+		else {
+			System.out.println("Необходимо ввести символ типа int");
+		}
 		sc.close();
 	}
 }
