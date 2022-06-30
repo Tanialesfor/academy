30446b13758626a9a0741920d34f85c45b01fb80
Tania <tanja.zirael@gmail.com>
2022-06-30 03:13:43 +0300

    lesson6 task10

--- academy/src/main/java/by/academy/lesson6/Task10.java
+++ academy/src/main/java/by/academy/lesson6/Task10.java
@@ -1,14 +1,18 @@
 package by.academy.lesson6;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 //Дана строка “Versions: Java  5, Java 6, Java   7, Java 8, Java 12.”   
 //Найти все подстроки "Java <X>" и распечатать их. 
 
 public class Task10 {
 	public static void main(String[] args) { 
-		String str="Versions: Java  5, Java 6, Java   7, Java 8, Java 12."; 
-		Pattern pattern = Pattern.compile("a*b"); 
-	    Matcher matcher = pattern.matcher(str); 
+		String str="Versions: Java  5, Java 6, Java   7, Java 8, Java 12.";
+		Pattern pattern = Pattern.compile("Java\\s+\\d{1,2}");
+		Matcher matcher = pattern.matcher(str);
+			while (matcher.find()) {
+			System.out.println(matcher.group());
+		} 
 	}
 }
