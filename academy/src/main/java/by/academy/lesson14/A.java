c1c24dbd56ec1e804cfd12d6bb73152bd8fe4842
Tania <tanja.zirael@gmail.com>
2022-07-28 16:22:03 +0300

    lesson 14

---
+++ academy/src/main/java/by/academy/lesson14/A.java
@@ -0,0 +1,8 @@
+package by.academy.lesson14;
+
+@Version(info = "A")
+public class A {
+	public void test() {
+
+	}
+}
---
+++ academy/src/main/java/by/academy/lesson14/AnnotationTest.java
@@ -0,0 +1,12 @@
+package by.academy.lesson14;
+
+@Version(info = "")
+public class AnnotationTest {
+	@Version(info = "3.3.3")
+	private Integer i;
+
+	@Version(info = "3.3.3")
+	public void main() {
+
+	}
+}
